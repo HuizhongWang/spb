f1e50dece6ae7dcc3be839f56c1cb9316673a5e5
HuizhongWang <minasopor23@gmail.com>
2024-02-01 08:55:45 +1300

    add part

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ from datetime import datetime
 import mysql.connector
 from mysql.connector import FieldType
 import connect
+import decimal
 
 app = Flask(__name__)
 
 dbconn = None
 connection = None
-job_item = None
+def get_flashed_messages():
+    pass
 
 # connect database
 def getCursor():
@@ -44,50 +46,48 @@ def currentjobs():
         jobList = connection.fetchall() 
         return render_template("currentjoblist.html", job_list = jobList)    
     else:
-        job_item = request.form["name"]
         return redirect(url_for("addjobs"))
 
 @app.route("/addjobs",methods = ["GET","POST"])    # technician add jobs
 def addjobs():
+    global get_flashed_messages
     connection = getCursor()
+
     # get job_id
     job_id = request.args.get('job_id') 
-  
     connection.execute("""select j.job_id,j.customer,j.job_date from job j
         left join job_service js on j.job_id=js.job_id
-        left join job_part jp on j.job_id=jp.job_id
-        where j.job_id= %s;""", (job_id,) )        
-  
+        where j.job_id= %s
+        group by j.job_id;""", (job_id,) )        
     job_detail_list = connection.fetchall() 
 
-    # get service name 
+    # get service info
     connection.execute("select * from service")
     service_list = connection.fetchall() 
 
-    # get part name
+    # get part info
     connection.execute("select * from part")
     part_list = connection.fetchall() 
     
-    # get input imformation
-    if request.method == 'POST':
+    if request.method == 'POST':    
+        # get input imformation
         service = request.form.get('service_name') 
         service_qty = request.form.get('service_qty') 
         part = request.form.get('part_name') 
         part_qty = request.form.get('part_qty') 
- 
-        if type(service_qty)==int and service_qty!=0:
+        qty = r'^[1-9]\d*$'
+        match_service = re.match(qty,service_qty)
+        match_part = re.match(qty,part_qty)
+
+        if service!="Open this select menu" and match_service:
+            print(service)
             connection.execute("select service_id from service where %s = service_name",(service,))
-            service_id = connection.fetchone() 
+            service_id = connection.fetchone()[0]
             connection.execute("insert into job_service value (%s,%s,%s)",(job_id,service_id,service_qty,))
-            connection.commit()
-            print(service_id)
-        elif type(part_qty)==int and part_qty!=0:
-            connection.execute("select part_id from part where %s = service_name",(part,))
-            part_id = connection.fetchone() 
+        elif part!="Open this select menu" and match_part:
+            connection.execute("select part_id from part where %s = part_name",(part,))
+            part_id = connection.fetchone()[0]
             connection.execute("insert into job_part value (%s,%s,%s)",(job_id,part_id,part_qty,))
-            connection.commit() 
-        # else:   
-        #     get_flashed_messages = "Qty must be an integer."
 
     return render_template("addjobs.html",job_detail_list=job_detail_list,service_list=service_list,part_list=part_list)    
 
@@ -120,6 +120,23 @@ def addservice():
 
 @app.route("/addpart",methods = ["GET","POST"]) 
 def addpart():
+    partname= request.form.get("partname")
+    partcost= str(request.form.get("partcost"))
+    cost = r'^\d{1,3}$'
+    match_cost = re.match(cost,partcost)
+    
+    if request.method == "POST":   # 验证不为空，是小数，名字没有重复
+        connection = getCursor()
+        connection.execute("select part_name from part where part_name= %s",(partname,))
+        part= connection.fetchone()[0] 
+
+        if partname != None and part != None and match_cost:
+            partcost = decimal.Decimal(partcost)
+            print(partcost,partname)
+            connection.execute("insert into part value(0,%s,%s)",(partname,partcost,))
+        else:
+            print("repeat")
+     
     return render_template("add_part.html")    
 
 @app.route("/schedule",methods = ["GET","POST"]) 
